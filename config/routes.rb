ce7ca2f4527b0b52efa23ca21d7cbd366d9c6170
aun <aun.sira@gmail.com>
2016-05-05 19:08:03 +0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tasks
       scope 'tasks/:id' do
-        put 'update_status', to: 'tasks#set_status'
+        patch 'update_status', to: 'tasks#set_status'
       end
     end
   end
